44f9fee90bf5aa743262bf52c4f8d90b5c5e32b4
Chris McCord <chris@chrismccord.com>
2012-07-13 22:21:27 -0400

    Tweak auth documentation for better display when opening in editor.

--- config/initializers/auth.rb
+++ config/initializers/auth.rb
@@ -1,12 +1,13 @@
-# ==================================================================================================
+# =============================================================================
 # config/initializers/auth.rb
-# ==================================================================================================
+# =============================================================================
 #
 # Labrador requires HTTP Basic Authentication.
 # Uncomment the following lines and set sensible credentials.
 #
-#  NOTE: HTTP Basic Authentication sends credentials in the clear without encryption -
-#  If you are accessing your Labrador process remotely, be sure it is on a network you trust.
+# NOTE: HTTP Basic Auth sends credentials in the clear without encryption -
+# If you are accessing your Labrador process remotely, 
+# be sure it is on a network you trust.
 
 
 # ENV['LABRADOR_USER'] = 'your_username'
