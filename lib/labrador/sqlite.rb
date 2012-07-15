353f03b45382cbad4d9047bf5572cba9bb3c4df0
Chris McCord <chris@chrismccord.com>
2012-07-14 23:21:54 -0400

    Add create, update, delete methods to sqlite driver.

--- lib/labrador/sqlite.rb
+++ lib/labrador/sqlite.rb
@@ -36,6 +36,38 @@ module Labrador
       ").map{|record| record.delete_if{|key, val| key.is_a?(Integer) } }
     end
 
+    def create(collection_name, data = {})
+      primary_key_name = primary_key_for(collection_name)
+      values = data.collect{|key, val| val }
+      fields = data.collect{|key, val| key.to_s }.join(", ")
+      prepared_values = (["?"]* data.keys.length).join(", ")      
+      query = session.prepare("
+        INSERT INTO #{collection_name}
+        (#{ fields })
+        VALUES (#{ prepared_values })
+      ")
+      query.execute(values)
+    end
+
+    def update(collection_name, id, data = {})
+      primary_key_name = primary_key_for(collection_name)
+      prepared_key_values = data.collect{|key, val| "#{key}=?" }.join(",")
+      values = data.values
+      values << id
+      query = session.prepare("
+        UPDATE #{collection_name}
+        SET #{ prepared_key_values }
+        WHERE #{primary_key_name}= ?
+      ")
+      query.execute(values)
+    end
+
+    def delete(collection_name, id)
+      primary_key_name = primary_key_for(collection_name)
+      query = session.prepare("DELETE FROM #{collection_name} WHERE #{primary_key_name}=?")
+      query.execute(id)
+    end
+
     def primary_key_for(collection_name)
       result = session.table_info(collection_name).select{|field| field["pk"] == 1 }.first
       result && result["name"]
