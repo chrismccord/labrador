d180c838a0719b337e027e3df7f0a890aa83347d
Chris McCord <chris@chrismccord.com>
2012-07-15 00:19:28 -0400

    Add CRUD actions to data controller.

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,16 +2,50 @@ class DataController < ApplicationController
 
   before_filter :find_applications  
   before_filter :find_adapters
+  around_filter :catch_crud_errors, only: [:create, :update, :destroy]
 
   def index    
     items = current_adapter.database.find(params[:collection], parse_options)
     render json: {
+      primary_key: current_adapter.database.primary_key_for(params[:collection]),
       fields: current_adapter.database.fields_for(items),
       items: items
     }
   end
 
+  def create
+    current_adapter.database.create(params[:collection], params[:data])
+    render json: { success: true }
+  end
+
+  def update
+    current_adapter.database.update(params[:collection], params[:id], params[:data])
+    render json: { success: true }
+  end
+
+  def destroy
+    current_adapter.database.delete(params[:collection], params[:id])
+    render json: { success: true }
+  end
+
   def collections
     render json: current_adapter.databse.collections
   end
+
+  private
+
+  def catch_crud_errors
+    begin
+      yield
+    rescue Exception => e
+      return render_json_error(e)
+    end
+  end
+
+  def render_json_error(error)
+    error_message = error.to_s
+    render json: {
+      error: error_message
+    }
+  end 
 end
