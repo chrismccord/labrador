26f78e056471d115e571cdcdd30c012cb5202802
Chris McCord <chris@chrismccord.com>
2012-07-24 00:29:37 -0400

    Refactor Adapter class and add propper connection management. Fix redundant connections.

--- lib/labrador/adapter.rb
+++ lib/labrador/adapter.rb
@@ -16,20 +16,28 @@ module Labrador
     def initialize(configuration_path, options = {})
       @configuration_path = File.expand_path(configuration_path)
       @errors = []
-
-      load_configuration
     end
 
-    def load_configuration
-      @configuration = case @configuration_path.split("/").last
+    def configuration
+      @configuration ||= case configuration_path.split("/").last
         when "database.yml" then database_yml_config
         when "mongoid.yml"  then mongoid_yml_config
-        else {}
       end
     end
 
+    def credentials
+      return unless configuration
+      {
+        host: configuration["host"],
+        user: configuration["username"],
+        database: configuration["database"],
+        password: configuration["password"],
+        socket: configuration["socket"]
+      }
+    end
+
     def database_yml_config
-      path = File.expand_path(@configuration_path)      
+      path = File.expand_path(configuration_path)      
       return unless File.exists?(path)       
       config = YAML.load(File.open(path))
 
@@ -37,7 +45,7 @@ module Labrador
     end
 
     def mongoid_yml_config
-      path = File.expand_path(@configuration_path)
+      path = File.expand_path(configuration_path)
       return unless File.exists?(path)
 
       config = YAML.load(File.open(path))
@@ -49,50 +57,56 @@ module Labrador
       config
     end
 
-    def is_valid?
-      @configuration
+    def valid?
+      !configuration.nil?
+    end
+
+    def connected?
+      database && database.connected?
     end
 
     def connect
-      return unless @configuration
-      conf = @configuration
-      credentials = {
-        host: conf["host"],
-        user: conf["username"],
-        database: conf["database"],
-        password: conf["password"],
-        socket: conf["socket"]
-      }
+      return unless configuration
+      @database = @@connections[name][db_name]    
+      return true if connected?
+
       begin
-        @database = case conf["adapter"]
-        when "mongodb"
-          @@connections[conf["adapter"]][conf["database"]] ||= Labrador::MongoDB.new(credentials)
-        when "postgresql"
-          @@connections[conf["adapter"]][conf["database"]] ||= Labrador::Postgres.new(credentials)
-        when "mysql", "mysql2"
-          @@connections[conf["adapter"]][conf["database"]] ||= Labrador::Mysql.new(credentials)
-        when "sqlite", "sqlite2", "sqlite3"
-          @@connections[conf["adapter"]][conf["database"]] ||= Labrador::Sqlite.new(credentials)
+        @database = case name
+        when "mongodb"        then MongoDB.new(credentials)
+        when "postgresql"     then Postgres.new(credentials)
+        when /^mysql(2)?$/    then Mysql.new(credentials)
+        when /^sqlite(2|3)?$/ then Sqlite.new(credentials)
         else
-          @errors << {
-            message: I18n.t('adapters.unsupported_adapter', adapter: conf["adapter"]),
-            adapter: conf["adapter"],
-            dump: conf
-          }
+          add_error(I18n.t('adapters.unsupported_adapter'))
+          nil
         end
       rescue Exception => e
-        @errors << {
-          message: e.to_s,
-          adapter: conf["adapter"],
-          dump: conf
-        }
+        add_error(e.to_s)
       end
 
-      @errors.empty?
+      @@connections[name][db_name] = @database
+    end
+
+    def disconnect
+      database.close if connected?
+      @@connections[name][db_name] = nil
     end
 
     def name
-      @configuration["adapter"]
+      configuration["adapter"] if configuration
+    end
+
+    def db_name
+      configuration["database"] if configuration
+    end
+
+    def add_error(message)
+      @errors << {
+        message: message,
+        adapter: name,
+        dump: configuration
+      }
     end
   end
 end
+
