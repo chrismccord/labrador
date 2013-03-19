0b0477fa8a2ff71e5e2424c292dbf212c2b2f6bc
Chris McCord <chris@chrismccord.com>
2013-03-18 20:42:15 -0400

    Add helper method for current database.

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,49 +1,52 @@
 class DataController < ApplicationController
 
   before_filter :find_applications  
-  before_filter :find_adapters
   around_filter :catch_errors, only: [:index, :create, :update, :destroy]
 
   def index    
-    items = current_adapter.database.find(collection, finder_params)
+    items = database.find(collection, finder_params)
     render json: {
-      primary_key: current_adapter.database.primary_key_for(collection),
+      primary_key: database.primary_key_for(collection),
       collection: collection,
-      fields: current_adapter.database.fields_for(items),
+      fields: database.fields_for(items),
       items: items
     }
   end
 
   def schema
-    items = current_adapter.database.schema(collection)
+    items = database.schema(collection)
     render json: {
       collection: collection,
-      fields: current_adapter.database.fields_for(items),
+      fields: database.fields_for(items),
       items: items
     }
   end
 
   def create
-    current_adapter.database.create(collection, data)
+    database.create(collection, data)
     render json: { success: true }
   end
 
   def update
-    current_adapter.database.update(collection, params[:id], data)
+    database.update(collection, params[:id], data)
     render json: { success: true }
   end
 
   def destroy
-    current_adapter.database.delete(collection, params[:id])
+    database.delete(collection, params[:id])
     render json: { success: true }
   end
 
   def collections
-    render json: current_adapter.databse.collections
+    render json: database.collections
   end
 
   
   private
+
+  def database
+    current_adapter.database
+  end
 
   def finder_params
     params.slice :limit, :order_by, :direction, :conditions, :skip
